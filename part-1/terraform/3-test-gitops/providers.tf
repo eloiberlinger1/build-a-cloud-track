bb6f91c98381733bf0a95ad9554a9a2f0d8651c8
Eloi BERLINGER <eloi.berling@protonmail.com>
2026-07-23 00:59:52 +0200

    chore: Switch to GitHub Actions Self-Hosted Runner & Local IPs

--- part-1/terraform/3-test-gitops/providers.tf
+++ part-1/terraform/3-test-gitops/providers.tf
@@ -12,13 +12,14 @@ terraform {
     bucket                      = "devstack-terraform-state"
     key                         = "terraform.tfstate"
     region                      = "us-east-1"
-    endpoints                   = { s3 = "https://s3.eloi-berlinger.ovh" }
+    endpoints                   = { s3 = "http://192.168.252.3:8080" }
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
     skip_metadata_api_check     = true
     skip_s3_checksum            = true
     use_path_style              = true
+    workspace_key_prefix        = "workspaces"
   }
 }
 
